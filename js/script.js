7edc26977553b691aa1b53c9a094214e6808593d
Leonardo Campos <leonardo.kiss66@live.com>
2023-12-24 18:40:27 -0300

    finaliado

--- js/script.js
+++ js/script.js
@@ -40,3 +40,22 @@ function initAnimacaoScroll2(){
   window.addEventListener('scroll', animaScroll);
 }
 initAnimacaoScroll2();
+
+function initAnimacaoScroll3(){
+  const liEfeito = document.querySelectorAll('.js-li-scroll3');
+  const windowMetade = window.innerHeight * 0.7;
+  
+  function animaScroll(){
+    liEfeito.forEach((li)=>{
+      const liTop = li.getBoundingClientRect().top - windowMetade;
+      if(liTop < 0) {
+        li.classList.add('ativo');
+      } else{
+        li.classList.remove('ativo');
+      }
+    });
+  }
+  
+  window.addEventListener('scroll', animaScroll);
+}
+initAnimacaoScroll3();
