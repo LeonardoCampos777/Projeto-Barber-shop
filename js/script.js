9846d116486fef29f824503f5b708ab6414a4a60
Leonardo Campos <leonardo.kiss66@live.com>
2023-12-24 17:26:33 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,42 @@
 //ANIMAÇÃO
-
 if(window.SimpleAnime){
   new SimpleAnime();
 }
+
+function initAnimacaoScroll1(){
+  const liEfeito = document.querySelectorAll('.js-li-scroll1');
+  const windowMetade = window.innerHeight * 0.7;
+  
+  function animaScroll(){
+    liEfeito.forEach((li)=>{
+      const liTop = li.getBoundingClientRect().top - windowMetade;
+      if(liTop < 0) {
+        li.classList.add('ativo');
+      } else{
+        li.classList.remove('ativo');
+      }
+    });
+  }
+  
+  window.addEventListener('scroll', animaScroll);
+}
+initAnimacaoScroll1();
+
+function initAnimacaoScroll2(){
+  const liEfeito = document.querySelectorAll('.js-li-scroll2');
+  const windowMetade = window.innerHeight * 0.7;
+  
+  function animaScroll(){
+    liEfeito.forEach((li)=>{
+      const liTop = li.getBoundingClientRect().top - windowMetade;
+      if(liTop < 0) {
+        li.classList.add('ativo');
+      } else{
+        li.classList.remove('ativo');
+      }
+    });
+  }
+  
+  window.addEventListener('scroll', animaScroll);
+}
+initAnimacaoScroll2();
