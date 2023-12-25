3c40d5735ab33373a44e4a792bd1d99d9df70fe6
Leonardo Campos <leonardo.kiss66@live.com>
2023-12-25 00:14:47 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,61 +1,58 @@
 //ANIMAÇÃO
-if(window.SimpleAnime){
+if (window.SimpleAnime) {
   new SimpleAnime();
 }
 
-function initAnimacaoScroll1(){
-  const liEfeito = document.querySelectorAll('.js-li-scroll1');
+function initAnimacaoScroll1() {
+  const liScroll = document.querySelector(".js-li-scroll1");
   const windowMetade = window.innerHeight * 0.7;
-  
-  function animaScroll(){
-    liEfeito.forEach((li)=>{
+
+  function animaScroll() {
+    const li = liScroll;
       const liTop = li.getBoundingClientRect().top - windowMetade;
-      if(liTop < 0) {
-        li.classList.add('ativo');
-      } else{
-        li.classList.remove('ativo');
+      if (liTop < 0) {
+        li.classList.add("ativo");
+      } else {
+        li.classList.remove("ativo");
       }
-    });
-  }
-  
-  window.addEventListener('scroll', animaScroll);
+    };
+
+  window.addEventListener("scroll", animaScroll);
 }
 initAnimacaoScroll1();
 
-function initAnimacaoScroll2(){
-  const liEfeito = document.querySelectorAll('.js-li-scroll2');
+function initAnimacaoScroll2() {
+  const liScroll = document.querySelector(".js-li-scroll2");
   const windowMetade = window.innerHeight * 0.7;
-  
-  function animaScroll(){
-    liEfeito.forEach((li)=>{
+
+  function animaScroll() {
+    const li = liScroll;
       const liTop = li.getBoundingClientRect().top - windowMetade;
-      if(liTop < 0) {
-        li.classList.add('ativo');
-      } else{
-        li.classList.remove('ativo');
+      if (liTop < 0) {
+        li.classList.add("ativo");
+      } else {
+        li.classList.remove("ativo");
       }
-    });
-  }
-  
-  window.addEventListener('scroll', animaScroll);
+    };
+
+  window.addEventListener("scroll", animaScroll);
 }
 initAnimacaoScroll2();
 
-function initAnimacaoScroll3(){
-  const liEfeito = document.querySelectorAll('.js-li-scroll3');
+function initAnimacaoScroll3() {
+  const liScroll = document.querySelector(".js-li-scroll3");
   const windowMetade = window.innerHeight * 0.7;
-  
-  function animaScroll(){
-    liEfeito.forEach((li)=>{
+
+  function animaScroll() {
+    const li = liScroll;
       const liTop = li.getBoundingClientRect().top - windowMetade;
-      if(liTop < 0) {
-        li.classList.add('ativo');
-      } else{
-        li.classList.remove('ativo');
+      if (liTop < 0) {
+        li.classList.add("ativo");
+      } else {
+        li.classList.remove("ativo");
       }
-    });
-  }
-  
-  window.addEventListener('scroll', animaScroll);
+    };
+
+  window.addEventListener("scroll", animaScroll);
 }
 initAnimacaoScroll3();
